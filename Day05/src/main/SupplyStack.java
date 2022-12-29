c06bf30fc810abdee55346377222a7371fe9627f
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2022-12-29 06:14:10 +0000

    Completed AdventOfCode Day 05 Challenge 2

--- Day05/src/main/SupplyStack.java
+++ Day05/src/main/SupplyStack.java
@@ -8,9 +8,22 @@ import java.util.Scanner;
 
 public class SupplyStack {
     public static void main(String[] args) throws Exception {
-        System.out.println(movingCrates(crateCreation(importFile("src/files/StacksCleaned.txt")),parseFileMovements(importFile("src/files/Operations.txt"))));
+        ArrayList<ArrayList<Character>> result=movingCrates(crateCreation(importFile("src/files/StacksCleaned.txt")),parseFileMovements(importFile("src/files/Operations.txt")));
+        String trueAnswer="";
+        for(int i=0;i<result.size();i++){
+            trueAnswer+=result.get(i).get(result.get(i).size()-1);
+        }
+
+        System.out.println(result);
+        System.out.println(trueAnswer);
     }
 
+    /**
+     * 
+     * @param fileLocation
+     * @return
+     * @throws FileNotFoundException
+     */
     public static ArrayList<String> importFile(String fileLocation) throws FileNotFoundException{
         ArrayList<String> file=new ArrayList<>();
         Scanner readFile=new Scanner(new File(fileLocation));
@@ -23,6 +36,11 @@ public class SupplyStack {
         return file;
     }
 
+    /**
+     * 
+     * @param importedFile
+     * @return
+     */
     public static ArrayList<ArrayList<Integer>> parseFileMovements(ArrayList<String> importedFile){
         ArrayList<ArrayList<Integer>> movesMake=new ArrayList<>();
 
@@ -37,6 +55,11 @@ public class SupplyStack {
         return movesMake;
     }
 
+    /**
+     * 
+     * @param importedFile
+     * @return
+     */
     public static ArrayList<ArrayList<Character>> crateCreation(ArrayList<String> importedFile){
         ArrayList<ArrayList<Character>> crates=new ArrayList<>();
 
@@ -53,17 +76,31 @@ public class SupplyStack {
         return crates;
     }
 
+    /**
+     * 
+     * @param crateValues
+     * @param movementValues
+     * @return
+     */
     public static ArrayList<ArrayList<Character>> movingCrates(ArrayList<ArrayList<Character>> crateValues,ArrayList<ArrayList<Integer>> movementValues){
         for(int i=0;i<movementValues.size();i++){
-            if(crateValues.get(movementValues.get(i).get(1)).size()<movementValues.get(i).get(0)){
-                movementValues.get(i).set(0,(crateValues.get(movementValues.get(i).get(1)).size()));
-            }
+            ArrayList<Character> valuesMove=new ArrayList<>();
 
             for(int j=0;j<movementValues.get(i).get(0);j++){
-                char valueToAdd=crateValues.get(movementValues.get(i).get(1)).get(crateValues.get(movementValues.get(i).get(1)).size()-1); // Pull Value to Move
-                crateValues.get(movementValues.get(i).get(2)).add(valueToAdd); // Add Value to Other Crate
-                crateValues.get(movementValues.get(i).get(1)).remove(crateValues.get(movementValues.get(i).get(1)).size()-1); // Remove Value Added to Other Crate
+                char valueToAdd=crateValues.get(movementValues.get(i).get(1)).get(crateValues.get(movementValues.get(i).get(1)).size()-1);
+                valuesMove.add(valueToAdd);
+                // crateValues.get(movementValues.get(i).get(2)).add(valueToAdd);
+                crateValues.get(movementValues.get(i).get(1)).remove(crateValues.get(movementValues.get(i).get(1)).size()-1);
             }
+            
+            while(!valuesMove.isEmpty()){
+                crateValues.get(movementValues.get(i).get(2)).add(valuesMove.get(valuesMove.size()-1));
+                valuesMove.remove(valuesMove.size()-1);
+            }
+
+            valuesMove.clear();
+
+            System.out.println(crateValues+"\t"+i);
         }
         return crateValues;
     }
